f824a5ae923ed5bbcce8c266ab4f02cc820e1bd5
js <>
2023-03-23 13:45:52 +0200

    retabify tpp's site.mk
    * tpp.scm (tpp):
      this was a case where tabs could not be replaced with spaces

--- unelo-proteomics/packages/tpp.scm
+++ unelo-proteomics/packages/tpp.scm
@@ -1,3 +1,18 @@
+;; Copyright (C) 2023
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (unelo-proteomics packages tpp)
   #:use-module (guix packages)
   #:use-module (ice-9 string-fun)
@@ -202,7 +217,7 @@ INSTALL_DIR=~a
 TPP_DATA_DIR=~a/share/tpp/
 # fixing the bug that $(BUILD_DIR) makes $(BUILD_WWW) but doesnt declare it as an output
 $(BUILD_WWW)/:
-        mkdir -p $@
+\tmkdir -p $@
 "
                             #$output #$output)))
                 (apply invoke "make" "extern-source" #$extern-all-configure make-flags)))
